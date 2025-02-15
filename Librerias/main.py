550f186a01aaa7135fc8000e25146dfd08a660cb
Antuag <joseguevaramur04@gmail.com>
2025-02-15 10:56:21 -0500

    Se agrega una funcion para realizar una suma con dos numeros

--- Librerias/main.py
+++ Librerias/main.py
@@ -1,3 +1,11 @@
 print ("Hola")
 print ("Felipe Buitrago")
 print ("Adios")
+
+
+# Funcion para realizar una suma
+
+def suma(a, b):
+    resultado = a + b
+    return resultado
+print("El resultado de la suma es:", suma(5, 2))
